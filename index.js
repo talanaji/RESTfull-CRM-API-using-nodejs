e0cf16dddd28db38934fc024561ae6ef864308f0
talanaji <talanaji@gmail.com>
2021-05-03 08:43:03 +0200

    setup mongo database

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 import express from "express";
 import routes from "./src/routes/crmRoutes";
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+
 const app = express();
 const port = 3000;
 routes(app);
+//mongoose connection 
+mongoose.Promise=global.Promise;
+mongoose.connect('mongodb://localhost/CRMdb',{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+}).then(() => console.log('MongoDB Connected...'))
+.catch((err) => console.log(err));
+//body parser setup
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+
 app.get("/", (req, res) => res.send(`Node & express running on port ${port}`));
 app.listen(port, () => console.log(`your server running on port ${port}`));
