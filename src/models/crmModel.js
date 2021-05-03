6824e48b4f3345ccdb4afed127c06e6bae56fb4c
talanaji <talanaji@gmail.com>
2021-05-03 08:54:04 +0200

    schema setup

---
+++ src/models/crmModel.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+const schema = mongoose.schema;
+export const ContactSchema = new schema({
+  firstName: { type: String, required: "Enter a first name" },
+  lastName: { type: String, required: "Enter a last name" },
+  email: { type: String },
+  company: { type: String },
+  phone: { type: Number },
+  created_date: { type: Date, default: Date.now },
+});
