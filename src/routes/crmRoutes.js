81a13cab7313f0f56b0220c740d676501e8700b9
talanaji <talanaji@gmail.com>
2021-05-03 08:20:16 +0200

    basic use for middelware

--- src/routes/crmRoutes.js
+++ src/routes/crmRoutes.js
@@ -1,10 +1,21 @@
-const routes = (app)=>{
-    app.route('/contact')
-    .get((req,res)=>res.send('GET request'))
-    .post((req,res)=>res.send('POST request'));
-    
-    app.route('/contact/:contactID')
-    .put((req,res)=>res.send('PUT request'))
-    .delete((req,res)=>res.send('DELETE request'));
-}
+const routes = (app) => {
+  app
+    .route("/contact")
+    .get(
+      (req, res, next) => {
+        console.log(`Request from ${req.originalUrl}`);
+        console.log(`Request type ${req.method}`);
+        next();
+      },
+      (req, res, next) => {
+        res.send("GET request");
+      }
+    )
+    .post((req, res) => res.send("POST request"));
+
+  app
+    .route("/contact/:contactID")
+    .put((req, res) => res.send("PUT request"))
+    .delete((req, res) => res.send("DELETE request"));
+};
 export default routes;
